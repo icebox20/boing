974b37faa7f1380fe07ff21a40efed8a0bec93cc
Arenevian <sunsnow246@gmail.com>
2020-05-08 16:24:10 -0700

    Added Title Screen Edited script names

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class MainMenu : MonoBehaviour
 {
     public string TutorialLevel;
     // Start is called before the first frame update
---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public string TitleScreen;
+    public gameObject pauseMenu;
+    public bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.Escape)){
+        if(isPaused){
+          Resume();
+        }else{
+          isPaused = true;
+          pauseMenu.SetActive(true);
+          Time.timeScale = 0f;
+        }
+      }
+    }
+
+    public void Resume(){
+      isPaused = false;
+      pauseMenu.SetActive(false);
+      Time.timeScale = 1f;
+    }
+
+    public void MainMenu(){
+      SceneManager.LoadScene(TitleScreen);
+    }
+}
