8e962727b0189a6a83b4864fe9fbc2cfa18e94cc
Arenevian <sunsnow246@gmail.com>
2020-05-16 16:10:26 -0700

    Connected Scenes

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -5,36 +5,34 @@ using UnityEngine.SceneManagement;
 
 public class Pause : MonoBehaviour
 {
-    public string TitleScreen;
-    public gameObject pauseMenu;
-    public bool isPaused;
-    // Start is called before the first frame update
-    void Start()
-    {
+  public string TitleScreen;
+  public GameObject pauseMenu;
+  public bool isPaused;
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-      if(Input.GetKeyDown(KeyCode.Escape)){
-        if(isPaused){
-          Resume();
-        }else{
-          isPaused = true;
-          pauseMenu.SetActive(true);
-          Time.timeScale = 0f;
-        }
+  }
+  // Update is called once per frame
+  void Update()
+  {
+    if(Input.GetKeyDown(KeyCode.Escape)){
+      if(isPaused){
+        Resume();
+      }else{
+        isPaused = true;
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
       }
     }
+  }
+  public void Resume(){
+    isPaused = false;
+    pauseMenu.SetActive(false);
+    Time.timeScale = 1f;
+  }
 
-    public void Resume(){
-      isPaused = false;
-      pauseMenu.SetActive(false);
-      Time.timeScale = 1f;
-    }
-
-    public void MainMenu(){
-      SceneManager.LoadScene(TitleScreen);
-    }
+  public void MainMenu(){
+    SceneManager.LoadScene(TitleScreen);
+  }
 }
